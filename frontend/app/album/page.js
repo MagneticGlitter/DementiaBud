1876d64ef2776a3b18b86ad65f8ba3901d018ba9
MagneticGlitter <92243872+MagneticGlitter@users.noreply.github.com>
2024-01-27 16:19:05 -0500

    push

--- frontend/app/album/page.js
+++ frontend/app/album/page.js
@@ -75,7 +75,7 @@ export default function Landing() {
             {/* Main 3 column grid */}
             <div className="grid grid-cols-1 items-start gap-4 lg:grid-cols-3 lg:gap-8">
               {/* Left column */}
-              <div className="grid grid-cols-1 gap-4 lg:col-span-2 overflow-hidden rounded-lg bg-white shadow p-6 h-[75vh] no-scrollbar">
+              <div className="grid grid-cols-1 gap-4 lg:col-span-2 overflow-hidden rounded-lg bg-white shadow p-6 h-full no-scrollbar">
                   <section aria-labelledby="section-1-title">
                     {/** ALBUM BEGINS HERE, CAESAR */}
                     <Album/>
