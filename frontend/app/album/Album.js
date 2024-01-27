b7977cf1f73881c02e69e42549f04894f5213057
MagneticGlitter <92243872+MagneticGlitter@users.noreply.github.com>
2024-01-27 16:50:00 -0500

    Update Album.js

--- frontend/app/album/Album.js
+++ frontend/app/album/Album.js
@@ -2,19 +2,19 @@ import React, { useState, useEffect } from "react";
 import ReactPlayer from "react-player";
 const Album = () => {
   const [folders, setFolders] = useState([]);
-
+  const [link, setLink] = useState("");
   useEffect(() => {
     // Fetch data or set your folder structure here
     const fetchedFolders = [
-      { id: 1, name: "Folder 1" },
-      { id: 2, name: "Folder 2" },
-      { id: 3, name: "Folder 3" },
-      { id: 4, name: "Folder 1" },
-      { id: 5, name: "Folder 2" },
-      { id: 6, name: "Folder 3" },
-      { id: 7, name: "Folder 1" },
-      { id: 8, name: "Folder 2" },
-      { id: 9, name: "Folder 3" },
+      { label:"Folder 1", link: "https://www.youtube.com/watch?v=paiO6M2wBqE"},
+      { label:"Folder 1", link: "https://www.youtube.com/watch?v=Et7TTfwvBFo"},
+      { label:"Folder 1", link: "www.google.com"},
+      { label:"Folder 1", link: "www.google.com"},
+      { label:"Folder 1", link: "www.google.com"},
+      { label:"Folder 1", link: "www.google.com"},
+      { label:"Folder 1", link: "www.google.com"},
+      { label:"Folder 1", link: "www.google.com"},
+      
       // Add more folders as needed
     ];
 
@@ -23,31 +23,43 @@ const Album = () => {
 
   return (
     
-    <>
-     <div>
-      {/* Video Player */}
-      <div className="player-wrapper">
-        <ReactPlayer
-          className="react-player"
-          url="https://www.youtube.com/watch?v=paiO6M2wBqE"
-          width="600px" // Set width to 1920
-          height="300px" // Set height to 1080
-          controls
-        />
-      </div>
-
-      {/* Folders */}
-      <div className="overflow-x-auto whitespace-nowrap">
-        <div className="flex p-4">
-          {folders.map((folder) => (
-            <div key={folder.id} className="mx-4 bg-pink-500 p-4 flex items-center justify-center text-white font-bold">
-              
-            </div>
-          ))}
-        </div>
-      </div>
+<>
+  <div style={{ overflowX: 'hidden' }}>
+    {/* Video Player */}
+    <div className="player-wrapper">
+      <ReactPlayer
+        className="react-player"
+        url={link}
+        width="600px"
+        height="300px"
+        controls
+      />
     </div>
-    </>
+
+
+  <div className="overflow-x-auto whitespace-nowrap mt-4">
+  <div className="flex p-4 flex-row">
+      {folders && folders.map((folder) => (
+        <>
+        <div className="flex flex-col items-center">
+            <img
+              src={`floppy3.png`}
+              width={180} // Adjust the width to your desired size
+              style={{ marginLeft: '50px', marginRight: '50px'}}
+            />
+            <a href="#" onClick={() => {setLink(folder.link)}} className="text-xs rounded-full border border-blue-700 text-blue-700 p-1">{folder.label}</a>
+        </div>
+      </>
+      ))}
+      
+  </div>
+</div>
+
+  </div>
+</>
+
+
+
   );
 };
 
