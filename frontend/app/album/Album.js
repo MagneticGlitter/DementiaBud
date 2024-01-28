36a90f6293b0d890a7bed558314121477bba0787
uzi311 <uzairidris4812@gmail.com>
2024-01-28 02:21:51 -0500

    push

--- frontend/app/album/Album.js
+++ frontend/app/album/Album.js
@@ -1,68 +1,51 @@
 import { Button } from "@mui/material";
 import React, { useState, useEffect } from "react";
+
 import ReactPlayer from "react-player";
-const Album = () => {
-  const [folders, setFolders] = useState([]);
-  const [link, setLink] = useState("");
-  useEffect(() => {
-    // Fetch data or set your folder structure here
-    const fetchedFolders = [
-      { label:"Folder 1", link: "https://www.youtube.com/watch?v=paiO6M2wBqE"},
-      { label:"Folder 1", link: "https://www.youtube.com/watch?v=Et7TTfwvBFo"},
-      { label:"Folder 1", link: "www.google.com"},
-      { label:"Folder 1", link: "www.google.com"},
-      { label:"Folder 1", link: "www.google.com"},
-      { label:"Folder 1", link: "www.google.com"},
-      { label:"Folder 1", link: "www.google.com"},
-      { label:"Folder 1", link: "www.google.com"},
-      
-      // Add more folders as needed
-    ];
 
-    setFolders(fetchedFolders);
-  }, []);
-
+const Album = (prop) => {
+  const [currLink, setCurrLink] = useState("./A_black_image.jpg.webp")
+  const labelToEmoji = {
+    "family": '🏠',
+    "education": '📚',
+    "romance": '❤️',
+    "sports": '⚽',
+    "accomplishment": '🏆',
+  };
   return (
     
-<>
+<div>
   <div style={{ overflowX: 'hidden' }}>
-    {/* Video Player */}
     <div className="player-wrapper">
       <ReactPlayer
         className="react-player"
-        url={link}
+        url={currLink}
         width="600px"
         height="300px"
         controls
       />
     </div>
 
-
-  <div className="overflow-x-auto whitespace-nowrap mt-4">
-  <div className="flex p-4 flex-row">
-      {folders && folders.map((folder) => (
-        <>
-        <div className="flex flex-col items-center">
+    <div className="overflow-x-auto whitespace-nowrap mt-4">
+      <div className="flex p-4 flex-row">
+        {prop.data && prop.data.map((row) => (
+          <div className="flex flex-col items-center" key={row.id}>
             <img
               src='polaroid.png'
               width={180} // Adjust the width to your desired size
               style={{ marginLeft: '50px', marginRight: '50px'}}
             />
             <Button sx={{ color: "lightpink", "&:hover": {backgroundColor: "lightcoral", color: "white"}, }}>
-            <a href="#" onClick={() => {setLink(folder.link)}}>{folder.label}</a>
+            <a href="#" onClick={() => {setCurrLink(row.link)}}> {labelToEmoji[row.label]} {row.summary}</a>
             </Button>
-        </div>
-      </>
-      ))}
+            </div>
+        ))}
+    </div>
+  </div>
+  </div>
+  </div>
+  
       
-  </div>
-</div>
-
-  </div>
-</>
-
-
-
   );
 };
 
