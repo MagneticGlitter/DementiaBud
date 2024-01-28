fc0997161e1a57b0624a3f7f463183a0c553ccf6
Marshal Guo <92243872+MagneticGlitter@users.noreply.github.com>
2024-01-28 05:05:54 -0500

    Revert "commiting home page"

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,43 +1,8 @@
 "use client"
-import Image from 'next/image';
-import { useRouter } from 'next/navigation';
-import { useUser } from '@auth0/nextjs-auth0/client';
-import { Button } from '@mui/material';
 
 export default function Home() {
-  const router = useRouter();
+  return (
+    <a href="/api/auth/login">Login</a>
 
-  const redirectToAlbum = () => {
-    router.push('/album');
-  };
-
-  const redirectToLogin = () => {
-    router.push('/api/auth/login');
-  }
-  const { user, error } = useUser()
-  console.log(user)
-  if (error) {
-    return (
-      <div>
-        {error.message}
-      </div>
-    )
-  }
-  if (user) {
-    return (
-      <div className='main-img'>
-       <Image src="/brain.png" width={650} height={650}/>
-       <Button sx={{ backgroundColor: "lightcoral", "&:hover": {backgroundColor: "lightcoral", color: "white"}, }} variant='contained' onClick={redirectToAlbum} className="home-button">Continue to Album</Button>
-     </div>
-    )
-  }
-  else {
-    return (
-      <div className='main-img'>
-       <Image src="/brain.png" width={650} height={650}/>
-       <Button sx={{ backgroundColor: "lightcoral", "&:hover": {backgroundColor: "lightcoral", color: "white"}, }} variant='contained' onClick={redirectToLogin} className="home-button">Click here to Login</Button>
-     </div>
-    )
-  }
+  );
 }
-
