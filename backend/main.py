7483600560b04297a762cd0cac62cc0ee2e0a2e0
CaesarSaleh <caesarssaleh@gmail.com>
2024-01-28 02:47:56 -0500

    fixed errors

--- backend/main.py
+++ backend/main.py
@@ -7,6 +7,8 @@ import numpy as np
 import cohere
 from cohere.responses.classify import Example
 import requests
+import supabase
+from supabase import create_client
 
 
 app = Flask(__name__)
@@ -52,8 +54,6 @@ def face_detect():
                 cv2.putText(imgOrignal, get_className(classIndex), (x, y-10), font,
                             0.75, (255, 255, 255), 1, cv2.LINE_AA)
 
-            cv2.putText(imgOrignal, str(round(probabilityValue*100, 2)) +
-                        "%", (180, 75), font, 0.75, (255, 0, 0), 2, cv2.LINE_AA)
         ret, buffer = cv2.imencode('.jpg', imgOrignal)
         # k=cv2.waitKey(1)
         # if k==ord('q'):
@@ -69,31 +69,16 @@ def add_cors_headers(response):
     response.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS'
     return response
 
+
 SUPABASE_URL = "https://fjnlnhzvvoirzuuiowoh.supabase.co/"
 SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZqbmxuaHp2dm9pcnp1dWlvd29oIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDYzNzUzMDgsImV4cCI6MjAyMTk1MTMwOH0.JwrXdA7OFkR-LFBFve6-wIa6gIKCJMxVo8dOwFxd68Q"
 SUPABASE_BUCKET_NAME = "memories"
 
+supabase = create_client(SUPABASE_URL, SUPABASE_ANON_KEY)
+
 @app.route('/')
 def index():
     return render_template('index_voice_commands.html')
-
-@app.route('/upload_video', methods=['POST'])
-def upload_video():
-    video_data = request.files['video']
-    
-    url = f"{SUPABASE_URL}/storage/v1/object/{SUPABASE_BUCKET_NAME}/videos/{video_data.filename}"
-    headers = {
-        "Content-Type": "video/mp4",  
-        "Authorization": f"Bearer {SUPABASE_ANON_KEY}",
-    }
-
-    with requests.post(url, headers=headers, files={'file': video_data}) as response:
-        if response.status_code == 200:
-            video_url = f"{SUPABASE_URL}/storage/v1/object/{SUPABASE_BUCKET_NAME}/videos/{video_data.filename}"
-            return jsonify({'video_url': video_url})
-        else:
-            return jsonify({'success': False, 'message': f'Failed to upload video. Status Code: {response.status_code}'})
-
 
 
 @app.route('/save_transcriptions', methods=['POST'])
@@ -101,18 +86,42 @@ def save_transcriptions():
     data = request.json
     transcriptions = data.get('transcriptions')
     transcriptions_string = '\n'.join(transcriptions)
+
     co = cohere.Client('5ZCwzEUBljEC37UbIpFLgCXtGpMW8XiXYtCiHVX8')
-    examples=[Example("Remember when we went to the park? Yeah! The one with the big slide and swings? That's the one. We chatted about school and your friends. I told you about my funny teacher and my best friend, Sarah. Your stories always make me smile. And you said I'm the best storyteller ever! You are, indeed. Our talks make my day. Mine too, especially when we share ice cream after.  Here's it's summary: In this sweet chat, you and your child reminisce about a fun park day, sharing laughter, stories, and the joy of post-play ice cream. It's a heartwarming snapshot of your close bond.", "family"), Example("Hey, remember that crazy exam week? Late-night study sessions were practically our second home! But hey, we nailed it! That feeling when the grades came in was priceless. I called you right away, and we celebrated like there was no tomorrow.",
-                        "education"), Example("That date night under the stars was incredible. Seriously, it felt like a scene from a movie. I still remember how we couldn't stop laughing. Best night ever. Agreed. We need more of those magical moments.", "romance"), Example("That game day was insane! I couldn't sit still. Your winning play was legendary. Nachos and victory talks afterward were the perfect way to end it. Let's catch another game soon!", "sports"), Example("Hitting that goal was a journey. The late nights, the hard work—it all paid off. I'll never forget the day it happened. We celebrated like champs. Your success is well-deserved. Cheers to more milestones!", "accomplishment"), Example("Our impromptu family picnic was a blast. Despite the squished sandwiches, we laughed non-stop. Your little sister's fascination with funny-shaped clouds made it even more memorable.", "family"),
-    Example("Late-night study sessions during exam week felt like our second home. The relief and joy upon receiving the grades were priceless. We celebrated like there was no tomorrow.", "education"),
-    Example("Our date night under the stars was magical. It felt straight out of a movie scene, and we couldn't stop laughing. Best night ever. Let's create more of those enchanting moments together.", "romance"),
-    Example("Game day was insane! I couldn't sit still, and your winning play was legendary. Nachos and victory talks afterward were the perfect way to end it. Looking forward to catching another game soon!", "sports"),
-    Example("Hitting that goal was a journey of late nights and hard work. The celebration was epic, and your success is well-deserved. Cheers to more milestones and triumphs!", "accomplishment")]
+    examples = [Example("Remember when we went to the park? Yeah! The one with the big slide and swings? That's the one. We chatted about school and your friends. I told you about my funny teacher and my best friend, Sarah. Your stories always make me smile. And you said I'm the best storyteller ever! You are, indeed. Our talks make my day. Mine too, especially when we share ice cream after.  Here's it's summary: In this sweet chat, you and your child reminisce about a fun park day, sharing laughter, stories, and the joy of post-play ice cream. It's a heartwarming snapshot of your close bond.", "family"), Example("Hey, remember that crazy exam week? Late-night study sessions were practically our second home! But hey, we nailed it! That feeling when the grades came in was priceless. I called you right away, and we celebrated like there was no tomorrow.",
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             "education"), Example("That date night under the stars was incredible. Seriously, it felt like a scene from a movie. I still remember how we couldn't stop laughing. Best night ever. Agreed. We need more of those magical moments.", "romance"), Example("That game day was insane! I couldn't sit still. Your winning play was legendary. Nachos and victory talks afterward were the perfect way to end it. Let's catch another game soon!", "sports"), Example("Hitting that goal was a journey. The late nights, the hard work—it all paid off. I'll never forget the day it happened. We celebrated like champs. Your success is well-deserved. Cheers to more milestones!", "accomplishment"), Example("Our impromptu family picnic was a blast. Despite the squished sandwiches, we laughed non-stop. Your little sister's fascination with funny-shaped clouds made it even more memorable.", "family"),
+                Example("Late-night study sessions during exam week felt like our second home. The relief and joy upon receiving the grades were priceless. We celebrated like there was no tomorrow.", "education"),
+                Example("Our date night under the stars was magical. It felt straight out of a movie scene, and we couldn't stop laughing. Best night ever. Let's create more of those enchanting moments together.", "romance"),
+                Example("Game day was insane! I couldn't sit still, and your winning play was legendary. Nachos and victory talks afterward were the perfect way to end it. Looking forward to catching another game soon!", "sports"),
+                Example("Hitting that goal was a journey of late nights and hard work. The celebration was epic, and your success is well-deserved. Cheers to more milestones and triumphs!", "accomplishment")]
     label = co.classify(inputs=[transcriptions_string], examples=examples)
     summary = co.generate(
         prompt="""I want you to give me a personalized description of the memory text of my life. For example, Here's a conversation: Remember when we went to the park? Yeah! The one with the big slide and swings? That's the one. We chatted about school and your friends. I told you about my funny teacher and my best friend, Sarah. Your stories always make me smile. And you said I'm the best storyteller ever! You are, indeed. Our talks make my day. Mine too, especially when we share ice cream after.  Here's it's summary: In this sweet chat, you and your child reminisce about a fun park day, sharing laughter, stories, and the joy of post-play ice cream. It's a heartwarming snapshot of your close bond.    The following is the memory text of my life:  """ + transcriptions_string)
-    print("Received transcriptions:", transcriptions_string)
-    return jsonify({label: label, summary: summary})
+
+    key_data = data.get('key')
+
+    response = supabase.table('memories').select().eq(
+        'key_column', key_data).execute()
+
+    if response.status_code == 200 and response.json()['count'] > 0:
+        existing_row = response.json()['data'][0]
+        video_url = 'arbitrary_link'
+        existing_row['link'] = video_url
+        supabase.table('memories').update(existing_row).eq(
+            'key_column', key_data).execute()
+    else:
+        video_url = 'arbitrary_link'
+        new_row = {
+            'label': label,
+            'summary': summary,
+            'conversation': transcriptions_string,
+            'link': video_url,
+            'key_column': key_data
+        }
+        supabase.from_('memories').upsert(
+            [new_row], on_conflict=['key_column']).execute()
+
+    return jsonify({'video_url': video_url})
 
 
 @app.route('/video_feed')
